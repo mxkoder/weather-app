b74ad1bc0080778781c1ed4474aa2bc83cd74b34
Agnes Beviz <agnes.beviz@gmail.com>
2022-09-15 22:29:22 +0100

    formatted date

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -23,9 +23,11 @@ import WeatherIcon from "react-icons-weather";
 function ForecastSummary(props) {
   const { date, description, icon, temperature } = props;
 
+  const formattedDate = new Date(date).toDateString();
+
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
-      <div className="forecast-summary__date">{date}</div>
+      <div className="forecast-summary__date">{formattedDate}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
         <WeatherIcon name="owm" iconId={icon} />
       </div>

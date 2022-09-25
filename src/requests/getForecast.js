808a1865038bdd9432ecfa2963e4f308a1fd13c6
Agnes Beviz <agnes.beviz@gmail.com>
2022-09-25 19:51:00 +0100

    tidying up

--- src/requests/getForecast.js
+++ src/requests/getForecast.js
@@ -8,16 +8,9 @@ const getForecast = (
 ) => {
   let endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
 
-  // eslint-disable-next-line no-console
-  console.log("searchText=====>", searchText);
-
-  // need to change back to 'if (searchText) when rest of app is working
   if (searchText) {
     endpoint += `?city=${searchText}`;
   }
-
-  // eslint-disable-next-line no-console
-  console.log("endpoint=====>", endpoint);
 
   axios.get(endpoint).then((response) => {
     setSelectedDate(response.data.forecasts[0].date);

c2fae3a72868e9600ba48529de6e28d560d72e75
Agnes Beviz <agnes.beviz@gmail.com>
2022-10-27 23:11:34 +0100

    wind direction arrow added

--- src/components/ForecastDetails.js
+++ src/components/ForecastDetails.js
@@ -1,11 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+  WiDirectionUp,
+  WiDirectionDownLeft,
+  WiDirectionDownRight,
+  WiDirectionDown,
+  WiDirectionLeft,
+  WiDirectionRight,
+  WiDirectionUpLeft,
+  WiDirectionUpRight,
+} from "react-icons/wi";
 import "../styles/ForecastDetails.css";
 
 function ForecastDetails({ forecast }) {
   const { date, temperature, wind, humidity } = forecast;
 
   const formattedDate = new Date(date).toDateString();
+
+  let windArrowDirection = <WiDirectionUp />;
+  if (wind.direction === "sw" || wind.direction === "ssw") {
+    windArrowDirection = <WiDirectionUpRight />;
+  }
+  if (wind.direction === "w" || wind.direction === "wsw") {
+    windArrowDirection = <WiDirectionRight />;
+  }
+  if (wind.direction === "nw" || wind.direction === "wnw") {
+    windArrowDirection = <WiDirectionDownRight />;
+  }
+  if (wind.direction === "n" || wind.direction === "nnw") {
+    windArrowDirection = <WiDirectionDown />;
+  }
+  if (wind.direction === "ne" || wind.direction === "nne") {
+    windArrowDirection = <WiDirectionDownLeft />;
+  }
+  if (wind.direction === "e" || wind.direction === "ene") {
+    windArrowDirection = <WiDirectionLeft />;
+  }
+  if (wind.direction === "se" || wind.direction === "ese") {
+    windArrowDirection = <WiDirectionUpLeft />;
+  }
 
   return (
     <div className="forecast-details" data-testid="forecast-summary">
@@ -23,7 +56,7 @@ function ForecastDetails({ forecast }) {
         Wind Speed: {wind.speed} mph
       </div>
       <div className="forecast-details__wind-direction">
-        Wind Direction: {wind.direction}
+        Wind Direction: {windArrowDirection}
       </div>
     </div>
   );
